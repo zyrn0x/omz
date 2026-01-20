c5783c3bbd658b453966c6b79f4cc73560f508fb
zyrn0x <adambenkhai34@gmail.com>
2026-01-20 11:25:09 +0100

    Update omz.lua

--- omz.lua
+++ omz.lua
@@ -47,14 +47,14 @@ local CoreGui = cloneref(game:GetService('CoreGui'))
 local Debris = cloneref(game:GetService('Debris'))
 
 local mouse = Players.LocalPlayer:GetMouse()
-local old_March = CoreGui:FindFirstChild('March')
+local old_Omz = CoreGui:FindFirstChild('Omz')
 
-if old_March then
-    Debris:AddItem(old_March, 0)
+if old_Omz then
+    Debris:AddItem(old_Omz, 0)
 end
 
-if not isfolder("March") then
-    makefolder("March")
+if not isfolder("Omz") then
+    makefolder("Omz")
 end
 
 
@@ -283,7 +283,7 @@ local Config = setmetatable({
     save = function(self: any, file_name: any, config: any)
         local success_save, result = pcall(function()
             local flags = HttpService:JSONEncode(config)
-            writefile('March/'..file_name..'.json', flags)
+            writefile('Omz/'..file_name..'.json', flags)
         end)
     
         if not success_save then
@@ -292,13 +292,13 @@ local Config = setmetatable({
     end,
     load = function(self: any, file_name: any, config: any)
         local success_load, result = pcall(function()
-            if not isfile('March/'..file_name..'.json') then
+            if not isfile('Omz/'..file_name..'.json') then
                 self:save(file_name, config)
         
                 return
             end
         
-            local flags = readfile('March/'..file_name..'.json')
+            local flags = readfile('Omz/'..file_name..'.json')
         
             if not flags then
                 self:save(file_name, config)
@@ -515,17 +515,17 @@ end
 
 
 function Library:create_ui()
-    local old_March = CoreGui:FindFirstChild('March')
+    local old_Omz = CoreGui:FindFirstChild('Omz')
 
-    if old_March then
-        Debris:AddItem(old_March, 0)
+    if old_Omz then
+        Debris:AddItem(old_Omz, 0)
     end
 
-    local March = Instance.new('ScreenGui')
-    March.ResetOnSpawn = false
-    March.Name = 'March'
-    March.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-    March.Parent = CoreGui
+    local Omz = Instance.new('ScreenGui')
+    Omz.ResetOnSpawn = false
+    Omz.Name = 'Omz'
+    Omz.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+    Omz.Parent = CoreGui
     
     local Container = Instance.new('Frame')
     Container.ClipsDescendants = true
@@ -538,7 +538,7 @@ function Library:create_ui()
     Container.Size = UDim2.new(0, 0, 0, 0)
     Container.Active = true
     Container.BorderSizePixel = 0
-    Container.Parent = March
+    Container.Parent = Omz
     
     local UICorner = Instance.new('UICorner')
     UICorner.CornerRadius = UDim.new(0, 10)
@@ -583,7 +583,7 @@ function Library:create_ui()
     ClientName.FontFace = Font.new('rbxasset://fonts/families/GothamSSm.json', Enum.FontWeight.SemiBold, Enum.FontStyle.Normal)
     ClientName.TextColor3 = Color3.fromRGB(152, 181, 255)
     ClientName.TextTransparency = 0.20000000298023224
-    ClientName.Text = 'March'
+    ClientName.Text = 'Omz'
     ClientName.Name = 'ClientName'
     ClientName.Size = UDim2.new(0, 31, 0, 13)
     ClientName.AnchorPoint = Vector2.new(0, 0.5)
@@ -662,7 +662,7 @@ function Library:create_ui()
     local UIScale = Instance.new('UIScale')
     UIScale.Parent = Container    
     
-    self._ui = March
+    self._ui = Omz
 
     local function on_drag(input: InputObject, process: boolean)
         if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then 
@@ -719,7 +719,7 @@ function Library:create_ui()
     end;
 
     function self:UIVisiblity()
-        March.Enabled = not March.Enabled;
+        Omz.Enabled = not Omz.Enabled;
     end;
 
     function self:change_visiblity(state: boolean)
@@ -738,7 +738,7 @@ function Library:create_ui()
     function self:load()
         local content = {}
     
-        for _, object in March:GetDescendants() do
+        for _, object in Omz:GetDescendants() do
             if not object:IsA('ImageLabel') then
                 continue
             end
@@ -1052,7 +1052,7 @@ function Library:create_ui()
                 ModuleName.Text = settings.title or "Skibidi"
             else
                 ModuleName.RichText = true
-                ModuleName.Text = settings.richtext or "<font color='rgb(255,0,0)'>March</font> user"
+                ModuleName.Text = settings.richtext or "<font color='rgb(255,0,0)'>Omz</font> user"
             end;
             ModuleName.Name = 'ModuleName'
             ModuleName.Size = UDim2.new(0, 205, 0, 13)
@@ -1400,7 +1400,7 @@ function Library:create_ui()
                     Body.Text = settings.text or "Skibidi"
                 else
                     Body.RichText = true
-                    Body.Text = settings.richtext or "<font color='rgb(255,0,0)'>March</font> user"
+                    Body.Text = settings.richtext or "<font color='rgb(255,0,0)'>Omz</font> user"
                 end
                 
                 Body.Size = UDim2.new(1, -10, 0, 20)
@@ -1470,7 +1470,7 @@ function Library:create_ui()
                     Body.Text = settings.text or "Skibidi" -- Default text
                 else
                     Body.RichText = true
-                    Body.Text = settings.richtext or "<font color='rgb(255,0,0)'>March</font> user" -- Default rich text
+                    Body.Text = settings.richtext or "<font color='rgb(255,0,0)'>Omz</font> user" -- Default rich text
                 end
             
                 Body.Size = UDim2.new(1, -10, 1, 0)
@@ -1501,7 +1501,7 @@ function Library:create_ui()
                         Body.Text = new_settings.text or "Skibidi" -- Default text
                     else
                         Body.RichText = true
-                        Body.Text = new_settings.richtext or "<font color='rgb(255,0,0)'>March</font> user" -- Default rich text
+                        Body.Text = new_settings.richtext or "<font color='rgb(255,0,0)'>Omz</font> user" -- Default rich text
                     end
                 end;
             
@@ -7681,17 +7681,17 @@ local PlayerCosmetics = player:create_module({
                 local Announcer = Player.PlayerGui:WaitForChild("announcer")
                 local Winner = Announcer:FindFirstChild("Winner")
                 if Winner then
-                    Winner.Text = Library._config._flags["announcer_text"] or "discord.gg/March"
+                    Winner.Text = Library._config._flags["announcer_text"] or "discord.gg/Omz"
                 end
                 Announcer.ChildAdded:Connect(function(Value)
                     if Value.Name == "Winner" then
                         Value.Changed:Connect(function(Property)
                             if Property == "Text" and Library._config._flags["Custom_Announcer"] then
-                                Value.Text = Library._config._flags["announcer_text"] or "discord.gg/March"
+                                Value.Text = Library._config._flags["announcer_text"] or "discord.gg/Omz"
                             end
                         end)
                         if Library._config._flags["Custom_Announcer"] then
-                            Value.Text = Library._config._flags["announcer_text"] or "discord.gg/March"
+                            Value.Text = Library._config._flags["announcer_text"] or "discord.gg/Omz"
                         end
                     end
                 end)
