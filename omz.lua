dc9624cc068faf45ee2c24922af01c5953bcfe90
zyrn0x <adambenkhai34@gmail.com>
2026-01-23 11:03:16 +0100

    Update omz.lua

--- omz.lua
+++ omz.lua
@@ -6971,8 +6971,4 @@ end
     t1:CreateDropdown({Description = "Special Type - Player", Callback = "", SpecialType = "Player"})
 end]]--
 
-task.delay(4, function() 
-	Luna:LoadAutoloadConfig()
-end)
-
 return Luna
